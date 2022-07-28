e569a88aaeff4d25925ab956cfb8c2c26948153e
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-28 20:16:42 +0530

    added style

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const cashGiven = document.querySelector('.cash-given')
 const checkBtn = document.querySelector('.check-button')
 const errorMsg = document.querySelector('.error-msg')
 const notesNumber = document.querySelectorAll('.notes-number')
+const cashGivenBtn = document.querySelector('.cash-given-btn')
+const cashGivenSec = document.querySelector('.cash-given-section')
 
 const availableNotes = [2000,500,100,20,10,5,1]
 
 hideMessage()
+
+cashGivenBtn.addEventListener('click', () => {
+    cashGivenSec.style.display = 'block'
+    cashGivenBtn.style.display= 'none'
+})
 
 checkBtn.addEventListener('click', ()=> {
     hideMessage()
