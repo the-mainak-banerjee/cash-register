d6fa98417980fcbb8b90cc4a0f116e67c12fdf9a
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-07-28 19:49:53 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+const billAmount = document.querySelector('.bill-amount')
+const cashGiven = document.querySelector('.cash-given')
+const checkBtn = document.querySelector('.check-button')
+const errorMsg = document.querySelector('.error-msg')
+const notesNumber = document.querySelectorAll('.notes-number')
+
+const availableNotes = [2000,500,100,20,10,5,1]
+
+hideMessage()
+
+checkBtn.addEventListener('click', ()=> {
+    hideMessage()
+    const amountLeft = cashGiven.value - billAmount.value
+
+    if(billAmount.value > 0){
+        if(amountLeft === 0) {
+            showError("Don't need to return anything")
+        }
+    
+        if(amountLeft > 0) {
+            calculateCash(amountLeft)
+        }
+    
+        if(amountLeft < 0){
+            showError('Do you want to wash dishes')
+        }
+    }else{
+        showError('Invalid Bill Amount')
+
+    }
+})
+
+function calculateCash(amount) {
+    for(i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amount / availableNotes[i])
+        amount %= availableNotes[i]
+        notesNumber[i].innerText = numberOfNotes
+    }
+}
+
+function hideMessage() {
+    errorMsg.style.display = 'none'
+}
+
+function showError(msg) {
+    errorMsg.style.display = 'block'
+    errorMsg.innerText = msg;
+}
